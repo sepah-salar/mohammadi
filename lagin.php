0bc44c6d9b16e5c13359d5853a65fe15bf46fbd6
sepah-salar <mohammad1386mohammad720@gmail.com>
2024-12-08 08:09:21 +0330

    version 6

--- lagin.php
+++ lagin.php
@@ -1,12 +1,64 @@
-<?php
-include("hed.html");
-?>
 
-<div id="div-lagin">
-    <p>لا fhnxfnfnگین</p>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    
+<head>
+    
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lagin.css">
+
+    
+   
+
+    <title>login dialog</title>
+</head>
+
+<body>
+    
+    
+<div class="main">
+<input type="checkbox" id="chktoggle" aria-hidden="true">
+
+    <div class="signup">
+   
+
+        <form action="">
+        <label for="chktoggle">sign up</label>
+            <input type="text"  placeholder="user name" required>
+            <input type="email"  placeholder="email" required>
+            <input type="password"  placeholder="password" required>
+            <button>signup</button>
+        </form>
+
+    </div>
+
+
+    <div class="login">
+        
+
+            <form action="">
+                <label for="chktoggle">login</label>
+            <input type="email"  placeholder="email" required>
+            <input type="password"  placeholder="password" required>
+            <button>login</button>
+        </form>
+
+    </div>
+    
+   
+
 </div>
 
-<?php
-include("fot.html");
-?>
+
+
+
+</body>
+</html>
+
+
+
 
